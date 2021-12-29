8ea8ff767d5ec0a4cb103acca23b0fd78b5d5522
godLei6 <603785348@qq.com>
2021-12-29 22:18:38 +0800

    恢复路由初始化

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -21,11 +21,11 @@ func Routers() *gin.Engine {
 	// VUE_APP_BASE_API = /
 	// VUE_APP_BASE_PATH = http://localhost
 	// 然后执行打包命令 npm run build。在打开下面4行注释
-	Router.LoadHTMLGlob("./dist/*.html") // npm打包成dist的路径
-	Router.Static("/favicon.ico", "./dist/favicon.ico")
-	Router.Static("/static", "./dist/assets")   // dist里面的静态资源
-	Router.Static("/assets", "./dist/assets")   // dist里面的静态资源
-	Router.StaticFile("/", "./dist/index.html") // 前端网页入口页面
+	//Router.LoadHTMLGlob("./dist/*.html") // npm打包成dist的路径
+	//Router.Static("/favicon.ico", "./dist/favicon.ico")
+	//Router.Static("/static", "./dist/assets")   // dist里面的静态资源
+	//Router.Static("/assets", "./dist/assets")   // dist里面的静态资源
+	//Router.StaticFile("/", "./dist/index.html") // 前端网页入口页面
 
 	Router.StaticFS(global.GVA_CONFIG.Local.Path, http.Dir(global.GVA_CONFIG.Local.Path)) // 为用户头像和文件提供静态地址
 	// Router.Use(middleware.LoadTls())  // 打开就能玩https了
