eb4738b68716fa589c529d99e848b9c495570e1b
godLei6 <603785348@qq.com>
2022-01-15 11:55:28 +0800

    修复新版编译后目录不对的问题

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -23,8 +23,9 @@ func Routers() *gin.Engine {
 	// 然后执行打包命令 npm run build。在打开下面4行注释
 	//Router.LoadHTMLGlob("./dist/*.html") // npm打包成dist的路径
 	//Router.Static("/favicon.ico", "./dist/favicon.ico")
-	//Router.Static("/static", "./dist/assets")   // dist里面的静态资源
 	//Router.Static("/assets", "./dist/assets")   // dist里面的静态资源
+	//Router.Static("/js", "./dist/js")           // dist里面的静态资源
+	//Router.Static("/gva", "./dist/gva")         // dist里面的静态资源
 	//Router.StaticFile("/", "./dist/index.html") // 前端网页入口页面
 
 	Router.StaticFS(global.GVA_CONFIG.Local.Path, http.Dir(global.GVA_CONFIG.Local.Path)) // 为用户头像和文件提供静态地址
