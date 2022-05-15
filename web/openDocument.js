10f35c40d828b6d8c147c91cb405f0c49a0f770e
godLei6 <603785348@qq.com>
2022-05-15 18:25:07 +0800

    doc: 更改打开的首页为iThings

--- web/openDocument.js
+++ web/openDocument.js
@@ -7,7 +7,7 @@
 
 var child_process = require('child_process')
 
-var url = 'https://www.gin-vue-admin.com'
+var url = 'https://ithings.pages.dev'
 var cmd = ''
 console.log(process.platform)
 switch (process.platform) {
